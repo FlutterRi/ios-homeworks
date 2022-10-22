11acace400ee84816ccf041cd2a37eda8c0013f7
Marina Mustafina <marina24mmm@yandex.ru>
2022-10-22 19:13:33 +0500

    creating Profile.storyboard and setting it as Main interface

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Липецкие on 22.10.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
